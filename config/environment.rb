d074ea9c4810165a014d35ad6fb3f3852ecc17ac
Jason <jason.laster.11@gmail.com>
2010-07-28 12:27:17 -0700

    gems fix

--- config/environment.rb
+++ config/environment.rb
@@ -19,9 +19,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "aasm", :source => "http://gems.github.com", :lib => 'aasm'
+  config.gem 'aasm', :source => "http://gemcutter.org", :version => "2.1.5"
   config.gem "facets"
-  config.gem ""
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
